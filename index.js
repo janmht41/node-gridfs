c83e06312b1ced2495db0849f096646c1abd5df4
Mohit Jain <janmht41@gmail.com>
2023-02-23 21:50:40 +0530

    test commit

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ let express = require("express");
 let app = express();
 const multer = require("multer");
 const { insertToDb, fetchFromDb } = require("./mongo.js");
-const fs = require("fs");
 const path = require("path");
-// let bodyParser = require("body-parser");
 
 const upload = multer({ dest: "uploads/" });
-
-// app.use(bodyParser.json());
 
 app.get("/image/:fileName", async function (req, res) {
   res.set("content-type", "image/png");
@@ -18,7 +14,6 @@ app.get("/image/:fileName", async function (req, res) {
 
 app.post("/image/upload", upload.single("file"), (req, res) => {
   const { file } = req;
-  // console.log("FILE", file);
   insertToDb(file, "abc", "asdad");
 
   return res.send("File Uploaded");
