4d169a30241bb5816cfde76b95aca9472bbb7107
Mohit Jain <janmht41@gmail.com>
2023-02-26 17:17:59 +0530

    separate log files

--- logconfig.js
+++ logconfig.js
@@ -2,16 +2,26 @@ import winston from "winston";
 const { combine, timestamp, json } = winston.format;
 import { config } from "dotenv";
 config();
-export const logger = winston.createLogger({
+
+const errorFilter = winston.format((info, opts) => {
+  return info.level === "error" ? info : false;
+});
+
+const infoFilter = winston.format((info, opts) => {
+  return info.level === "info" ? info : false;
+});
+
+const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.File({
-      filename: "./logs/combined.log",
+      filename: "combined.log",
     }),
     new winston.transports.File({
-      filename: "./logs/app-error.log",
+      filename: "app-error.log",
       level: "error",
+      format: combine(errorFilter(), timestamp(), json()),
     }),
     new winston.transports.File({
       filename: "app-info.log",
